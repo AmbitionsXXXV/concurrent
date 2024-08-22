d0f83903c7272a74d20eb6e0d0d9090e5dd28139
etcetera <yilonglei790@gmail.com>
2024-08-22 17:09:37 +0800

    feat: 结构修改

--- examples/cmetrics.rs
+++ examples/cmetrics.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use concurrency::Metrics;
+use concurrency::CmapMetrics;
 use rand::Rng;
 use std::{thread, time::Duration};
 
@@ -7,11 +7,9 @@ const N: usize = 2;
 const M: usize = 4;
 
 fn main() -> Result<()> {
-  let metrics = Metrics::new();
+  let metrics = CmapMetrics::new();
 
   // start N workers and M requesters
-
-  println!("{:?}", metrics);
 
   for idx in 0..N {
     task_worker(idx, metrics.clone())?; // Metrics {data: Arc::clone(&metrics.data)}
@@ -27,32 +25,31 @@ fn main() -> Result<()> {
   }
 }
 
-fn task_worker(idx: usize, metrics: Metrics) -> Result<()> {
+fn task_worker(idx: usize, metrics: CmapMetrics) -> Result<()> {
   thread::spawn(move || {
     loop {
       // do long term stuff
       let mut rng = rand::thread_rng();
+
       thread::sleep(Duration::from_millis(rng.gen_range(100..5000)));
       metrics.inc(format!("call.thread.worker.{}", idx))?;
     }
-
     #[allow(unreachable_code)]
     Ok::<_, anyhow::Error>(())
   });
-
   Ok(())
 }
 
-fn request_worker(metrics: Metrics) -> Result<()> {
+fn request_worker(metrics: CmapMetrics) -> Result<()> {
   thread::spawn(move || {
     loop {
       // process requests
       let mut rng = rand::thread_rng();
+
       thread::sleep(Duration::from_millis(rng.gen_range(50..800)));
       let page = rng.gen_range(1..5);
       metrics.inc(format!("req.page.{}", page))?;
     }
-
     #[allow(unreachable_code)]
     Ok::<_, anyhow::Error>(())
   });
